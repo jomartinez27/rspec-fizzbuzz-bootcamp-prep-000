0617c184501bb7c278ec850419e040341bcfa76f
jomartinezmarquez@gmail.com <jomartinezmarquez@gmail.com>
2018-06-17 03:12:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,4 +3,5 @@
 def fizzbuzz(num)
   return 'Fizz' if (num % 3 == 0) && (num % 5 != 0)
   return 'Buzz' if (num % 5 == 0) && (num % 3 != 0)
+  return 'FizzBuzz' if (num % 3 == 0) && (num % 5 == 0)
 end
