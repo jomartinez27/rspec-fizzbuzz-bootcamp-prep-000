d6dd406f6011837ab7a07a610f45cfe1e63432ee
jomartinezmarquez@gmail.com <jomartinezmarquez@gmail.com>
2018-06-17 03:10:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(num)
+  return 'Fizz' if (num % 3 == 0) && (num % 5 != 0)
 end
